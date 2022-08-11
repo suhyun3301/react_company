445020654b3982b1f819029d2170fb71d11bf0ae
suhyun3301 <iam3301@naver.com>
2022-08-12 01:20:10 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { Route } from 'react-router-dom'
+
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
+
+import Visual from './components/main/Visual'
+import Content from './components/main/Content'
+
+import Department from './components/sub/Department'
+import Community from './components/sub/Community'
+import Gallery from './components/sub/Gallery'
+import Youtube from './components/sub/Youtube'
+import Location from './components/sub/Location'
+import Members from './components/sub/Members'
+
 import './scss/style.scss'
 
 function App() {
-  return <h1>Hello</h1>
+  return (
+    <>
+      <Header />
+      <Route exact path="/">
+        <Visual />
+        <Content />
+      </Route>
+
+      <Route path="/community" component={Department} />
+      <Route path="/community" component={Community} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/youtube" component={Youtube} />
+      <Route path="/location" component={Location} />
+      <Route path="/location" component={Members} />
+
+      <Footer />
+    </>
+  )
 }
 
 export default App
