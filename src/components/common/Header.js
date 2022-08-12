41613612de3f860fcbea97ba75347d3605dbbb6a
suhyun3301 <iam3301@naver.com>
2022-08-12 09:05:45 +0900

    header Link, NavLink 연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,33 @@
+import { Link, NavLink } from 'react-router-dom'
 function Header() {
-  return <div>Header</div>
+  return (
+    <header>
+      <h1>
+        <Link to="/">Logo</Link>
+      </h1>
+
+      <nav>
+        <li>
+          <NavLink to="/department">Department</NavLink>
+        </li>
+        <li>
+          <NavLink to="/community">Community</NavLink>
+        </li>
+        <li>
+          <NavLink to="/gallery">Gallery</NavLink>
+        </li>
+        <li>
+          <NavLink to="/youtube">Location</NavLink>
+        </li>
+        <li>
+          <NavLink to="/location">Youtube</NavLink>
+        </li>
+        <li>
+          <NavLink to="/members">Members</NavLink>
+        </li>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
