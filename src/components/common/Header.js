319c6cf265711372dfb32c44dbc399bef46fc5e4
suhyun3301 <iam3301@naver.com>
2022-08-13 00:33:58 +0900

    header 스타일링

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,31 +1,60 @@
 import { Link, NavLink } from 'react-router-dom'
+import { faTrello } from '@fortawesome/free-brands-svg-icons'
+import { faApple } from '@fortawesome/free-brands-svg-icons'
+import { faGooglePlay } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Header() {
   return (
-    <header>
-      <h1>
-        <Link to="/">Logo</Link>
-      </h1>
+    <header className="header">
+      <div className="inner">
+        <h1 className="logo">
+          <Link to="/">
+            <FontAwesomeIcon icon={faTrello} className="icon" />
+            <strong>Trello</strong>
+          </Link>
+        </h1>
 
-      <nav>
-        <li>
-          <NavLink to="/department">Department</NavLink>
-        </li>
-        <li>
-          <NavLink to="/community">Community</NavLink>
-        </li>
-        <li>
-          <NavLink to="/gallery">Gallery</NavLink>
-        </li>
-        <li>
-          <NavLink to="/youtube">Location</NavLink>
-        </li>
-        <li>
-          <NavLink to="/location">Youtube</NavLink>
-        </li>
-        <li>
-          <NavLink to="/members">Members</NavLink>
-        </li>
-      </nav>
+        <nav className="menu">
+          <ul className="gnb">
+            <li className="item">
+              <NavLink to="/department">Department</NavLink>
+            </li>
+            <li className="item">
+              <NavLink to="/community">Community</NavLink>
+            </li>
+            <li className="item">
+              <NavLink to="/gallery">Gallery</NavLink>
+            </li>
+            <li className="item">
+              <NavLink to="/youtube">Youtube</NavLink>
+            </li>
+            <li className="item">
+              <NavLink to="/location">Contact Us</NavLink>
+            </li>
+            <li className="item">
+              <NavLink to="/members">Join</NavLink>
+            </li>
+          </ul>
+
+          <ul className="etc">
+            <li>Privacy Policy</li>
+            <li>Terms</li>
+            <li>Cookie Settings</li>
+          </ul>
+
+          <div className="app">
+            <p>Trello also works great on your smaller screen.</p>
+            <div className="app-group">
+              <Link to="/">
+                <FontAwesomeIcon icon={faApple} />
+              </Link>
+              <Link to="/">
+                <FontAwesomeIcon icon={faGooglePlay} />
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
   )
 }
