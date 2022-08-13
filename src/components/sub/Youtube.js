a1677a673538de197667a7a2ad6d03c59d1ccbfe
suhyun3301 <iam3301@naver.com>
2022-08-13 17:45:41 +0900

    Youtube : API호출 후 리스트 출력

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,12 +1,48 @@
 import SubLayout from '../common/SubLayout'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Youtube() {
+  const [Videos, setVideos] = useState([])
   const subtitle = {
     title: 'Trello Views',
     p: 'Trusted by millions, Trello is the visual collaboration tool that creates a shared perspective on any project.',
   }
 
-  return <SubLayout name="youtube" sub={subtitle}></SubLayout>
+  useEffect(() => {
+    const key = 'AIzaSyDSdFyxbZ_BnbSiuPS3M6Ug9mM75LfRyJg'
+    const playList = 'PL_zwLMPR5YRLPV-Ni5MG2mds5p6RwupoD'
+    const num = 10
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playList}&maxResults=${num}`
+
+    axios.get(url).then((json) => {
+      setVideos(json.data.items)
+    })
+  }, [])
+
+  return (
+    <SubLayout name={'youtube'} sub={subtitle}>
+      {Videos.map((item, i) => {
+        return (
+          <article key={i}>
+            <div className="img-box">
+              <img
+                src={item.snippet.thumbnails.standard.url}
+                alt={item.snippet.title}
+              />
+            </div>
+
+            <strong>{item.snippet.videoOwnerChannelTitle}</strong>
+            <h3>{item.snippet.title}</h3>
+            <p>{item.snippet.description}</p>
+            <span>
+              {item.snippet.publishedAt.split('T')[0].replace(/-/gi, '.')}
+            </span>
+          </article>
+        )
+      })}
+    </SubLayout>
+  )
 }
 
 export default Youtube
