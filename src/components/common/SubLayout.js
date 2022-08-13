4ed493b4bec0ae1615a31faf5f87e9c58ded7ce4
suhyun3301 <iam3301@naver.com>
2022-08-13 15:14:03 +0900

    Sub : 모션을 넣기 위해 useEffect 사용하여 마운트됬을 때 클래스 on 추가

--- src/components/common/SubLayout.js
+++ src/components/common/SubLayout.js
@@ -1,7 +1,9 @@
 import Footer from './Footer'
+import { useEffect, useRef } from 'react'
 
 function SubLayout({ name, sub, children }) {
   let title = name
+  const section = useRef(null)
 
   if (title === 'location') {
     title = `contact us`
@@ -9,8 +11,12 @@ function SubLayout({ name, sub, children }) {
     title = 'join'
   }
 
+  useEffect(() => {
+    section.current.classList.add('on')
+  }, [])
+
   return (
-    <section className={`container ${name}`}>
+    <section className={`container ${name}`} ref={section}>
       <div className="title-box">
         <h1>{title}</h1>
         <h2>{sub.title}</h2>
