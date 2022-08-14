0d1ff1c255cbfa5f7af1b469d238a8f5b01bf7f6
suhyun3301 <iam3301@naver.com>
2022-08-14 18:11:49 +0900

    Department : json 호출 뒤 리스트 출력

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,4 +1,6 @@
 import SubLayout from '../common/SubLayout'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Department() {
   const subtitle = {
@@ -6,7 +8,26 @@ function Department() {
     p: 'What’s behind the boards.',
   }
 
-  return <SubLayout name="department" sub={subtitle}></SubLayout>
+  const [Members, setMembers] = useState([])
+  useEffect(() => {
+    axios.get(process.env.PUBLIC_URL + '/DB/members.json').then((json) => {
+      setMembers(json.data.members)
+    })
+  }, [])
+
+  return (
+    <SubLayout name="department" sub={subtitle}>
+      {Members.map((member, i) => {
+        return (
+          <article key={i}>
+            <div className="member-thum">
+              <img src={`${process.env.PUBLIC_URL}/img/${member.img}`} alt="" />
+            </div>
+          </article>
+        )
+      })}
+    </SubLayout>
+  )
 }
 
 export default Department
