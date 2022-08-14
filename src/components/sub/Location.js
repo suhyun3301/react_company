faa3d8d9b49d3fdb9e7a6945df3dbe15377ef0ed
suhyun3301 <iam3301@naver.com>
2022-08-14 16:15:37 +0900

    Location :  kakao map api활용해 지도 출력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,12 +1,38 @@
 import SubLayout from '../common/SubLayout'
+import { useRef, useEffect } from 'react'
 
 function Location() {
   const subtitle = {
     title: 'Contact support',
     p: 'For specific account or billing questions, security concerns, or answers not available on our help site, we’re happy to help.',
   }
+  const { kakao } = window
+  const mapBox = useRef(null)
+  const mapOption = {
+    center: new kakao.maps.LatLng(37.394776, 127.11116),
+    level: 3,
+  }
 
-  return <SubLayout name="location" sub={subtitle}></SubLayout>
+  const imgSrc = process.env.PUBLIC_URL + '/img/trello-map-icon.svg'
+  const imgSize = new kakao.maps.Size(30, 30)
+  const imgOption = { offset: new kakao.maps.Point(15, 30) }
+  const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption)
+
+  const marker = new kakao.maps.Marker({
+    position: mapOption.center,
+    image: markerImage,
+  })
+
+  useEffect(() => {
+    const map = new kakao.maps.Map(mapBox.current, mapOption)
+    marker.setMap(map)
+  }, [])
+
+  return (
+    <SubLayout name={'location'} sub={subtitle}>
+      <div id="map" ref={mapBox}></div>
+    </SubLayout>
+  )
 }
 
 export default Location
