6a1b0173351220b68404636ae5f9f06175f649e9
suhyun3301 <iam3301@naver.com>
2022-08-15 23:47:09 +0900

    Footer : 구조 및 스타일 작업

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,24 @@
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <ul>
+        <li>Privacy Policy</li>
+        <li>Terms</li>
+        <li>Cookie Settings</li>
+        <li>Copyright &copy; 2022 Atlassian</li>
+      </ul>
+
+      <form className="footer-form">
+        <FontAwesomeIcon icon={faGlobe} className="icon-globe" />
+        <select name="language" id="">
+          <option value="eng">Eng</option>
+          <option value="kor">Kor</option>
+        </select>
+      </form>
+    </footer>
+  )
 }
 
 export default Footer
