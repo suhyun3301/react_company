20ffc98712fb6c7ca136cbfcf601ac4a8c9b7fee
suhyun3301 <iam3301@naver.com>
2022-08-19 00:38:15 +0900

    Gallery: 스타일 작업

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -8,18 +8,54 @@ function Gallery() {
     p: 'PRODUCTS & NEWS',
   }
 
+  const [Items, setItems] = useState([])
+
   useEffect(() => {
     const key = 'fc53ebefe80f178c0979785406ddb3d5'
-    const num = 20
+    const num = 100
     const method_interest = 'flickr.interestingness.getList'
     const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`
 
     axios.get(url).then((json) => {
-      console.log(json.data)
+      setItems(json.data.photos.photo)
     })
   }, [])
 
-  return <SubLayout name="gallery" sub={subtitle}></SubLayout>
+  return (
+    <SubLayout name="gallery" sub={subtitle}>
+      {Items.map((item, i) => {
+        return (
+          <article key={i} className="flicker">
+            <div className="img-box">
+              <img
+                src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+                alt={item.title}
+              />
+
+              <div className="info">
+                <div className="avatar">
+                  <img
+                    src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+                    alt={item.owner}
+                    onError={(e) => {
+                      e.target.setAttribute(
+                        'src',
+                        'https://www.flickr.com/images/buddyicon.gif'
+                      )
+                    }}
+                  />
+                </div>
+                <div className="info-text">
+                  <p className="title">{item.title}</p>
+                  <span>{item.owner}</span>
+                </div>
+              </div>
+            </div>
+          </article>
+        )
+      })}
+    </SubLayout>
+  )
 }
 
 export default Gallery
