1a8ee6807e679afa675d13581ab8719e678d41db
suhyun3301 <iam3301@naver.com>
2022-08-20 14:14:02 +0900

    Gallery : userID로 검색 추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -8,12 +8,15 @@ function Gallery() {
   const [Items, setItems] = useState([])
   const [Index, setIndex] = useState(0)
   const [Tag, setTag] = useState([])
+
   const getFlickr = async (opt) => {
     const key = 'fc53ebefe80f178c0979785406ddb3d5'
     const num = 100
     const extras = 'tags, description, views'
     const method_search = 'flickr.photos.search'
     const method_interest = 'flickr.interestingness.getList'
+    const method_user = 'flickr.people.getPhotos'
+
     let url = ''
     if (opt.type === 'interest') {
       url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1`
@@ -21,6 +24,10 @@ function Gallery() {
 
     if (opt.type === 'search') {
       url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags=${opt.tag}`
+    }
+
+    if (opt.type === 'user') {
+      url = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&extras=${extras}&format=json&nojsoncallback=1&tags1&user_id=${opt.owner}`
     }
 
     await axios.get(url).then((json) => {
@@ -63,7 +70,14 @@ function Gallery() {
 
           <div className="info">
             <h1 className="title">{Items[i].title}</h1>
-            <span className="owner">{Items[i].owner}</span>
+            <span
+              className="owner"
+              onClick={(e) => {
+                getFlickr({ type: 'user', owner: e.target.innerText })
+              }}
+            >
+              {Items[i].owner}
+            </span>
             <strong className="views">
               <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
               {Items[i].views}
