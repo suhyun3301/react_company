9be4c1a229181cfaaec8dee9b5e55b4f04713d48
suhyun3301 <iam3301@naver.com>
2022-08-26 15:56:25 +0900

    최신 파일업로드

--- src/components/main/Content.js
+++ src/components/main/Content.js
@@ -1,5 +1,60 @@
 function Content() {
-  return <div>Content</div>
+  return (
+    <section className="main-box content">
+      <h1>
+        Trello makes it easy to organize and track individual or team tasks in
+        one place
+      </h1>
+
+      <div className="wrap">
+        <p>
+          Trello’s advanced checklists give you the power to add more context to
+          your tasks by assigning team members and due dates to checklist items,
+          increasing visibility and keeping your team accountable. <br />
+          Prioritize with labels to identify what is high, medium, or low
+          priority and bring an extra layer of shared understanding to your
+          tasks. You can even filter by a certain label to only view cards
+          associated with the label you want to view.
+        </p>
+
+        <p>
+          Trello’s advanced checklists give you the power to add more context to
+          your tasks by assigning team members and due dates to checklist items,
+          increasing visibility and keeping your team accountable. <br />
+          Prioritize with labels to identify what is high, medium, or low
+          priority and bring an extra layer of shared understanding to your
+          tasks. You can even filter by a certain label to only view cards
+          associated with the label you want to view.
+        </p>
+
+        <div className="count">
+          <div>
+            <span>01</span>
+            <h2>
+              12 Power-ups to transform Trello into a powerful task management
+              software
+            </h2>
+            <p>
+              Learn the best Power-Ups, integrations, and features to help you
+              and your team move tasks along effortlessly.
+            </p>
+          </div>
+
+          <div>
+            <span>02</span>
+            <h2>
+              12 Power-ups to transform Trello into a powerful task management
+              software
+            </h2>
+            <p>
+              Learn the best Power-Ups, integrations, and features to help you
+              and your team move tasks along effortlessly.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Content
---
+++ src/components/main/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function News() {
+  return (
+    <section className="main-box news">
+      <h1>
+        It’s more than work. <br />
+        It’s a way of working together.
+      </h1>
+    </section>
+  )
+}
+
+export default News
